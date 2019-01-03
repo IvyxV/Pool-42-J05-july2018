16f56154dc820e414294bb32dc42947c140908f3
Boujema <vincent.boujema@gmail.com>
2019-01-03 11:10:05 +0100

    Création J05

---
+++ ex10/ft_strcapitalize.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viboujem <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/14 17:39:26 by viboujem          #+#    #+#             */
+/*   Updated: 2018/07/17 15:56:14 by viboujem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+
+	i = 0;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			i++;
+		}
+		else
+			while ((str[i] >= '0' && str[i] <= '9')
+					|| (str[i] >= 'a' && str[i] <= 'z'))
+				i++;
+		while (str[i] >= 'a' && str[i] <= 'z')
+			i++;
+		i++;
+	}
+	return (str);
+}
